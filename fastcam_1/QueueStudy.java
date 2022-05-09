a27a63037f2a36f2209361481a73f291f49dc8dd
Anjeongkyun <dkswjdrbs12@naver.com>
2022-05-09 22:49:19 +0900

    Queue 클래스 작성

--- fastcam_1/QueueStudy.java
+++ fastcam_1/QueueStudy.java
@@ -1,17 +1,39 @@
 package fastcam_1;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class QueueStudy {
     public static void main(String[] args) {
-        Queue<Integer> queue_int = new LinkedList<Integer>();
-        Queue<String> queue_str = new LinkedList<String>();
 
-        //queue 데이터 add, offer 추가. -> Enqueue
-        queue_int.add(1);
-        queue_int.offer(2);
+        MyQueue<Integer> mq = new MyQueue<Integer>();
+        mq.enqueue(1);
+        mq.enqueue(2);
+        mq.enqueue(3);
+        System.out.println(mq.dequeue());
+        System.out.println(mq.dequeue());
+        System.out.println(mq.dequeue());
 
-        System.out.println(queue_int);
+    }
+
+    public static class MyQueue<T> {
+        private ArrayList<T> queue = new ArrayList<T>();
+
+        public void enqueue(T item){
+            queue.add(item);
+        }
+
+        public T dequeue(){
+            if(queue.isEmpty())
+                return null;
+
+            return queue.remove(0);
+        }
+
+        public boolean isEmpty(){
+            return queue.isEmpty();
+        }
+
     }
 }
