1958e15d5467e09ac9996a431bd9257cb54a171e
Anjeongkyun <dkswjdrbs12@naver.com>
2022-05-22 22:29:24 +0900

    내가 작성한 버블 정렬

---
+++ SortStudy/selfBubble.java
@@ -0,0 +1,36 @@
+package SortStudy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class selfBubble {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> testData = new ArrayList<Integer>();
+
+        for(int i = 0; i< 50; i++){
+            testData.add((int)(Math.random() * 100));
+        }
+
+        System.out.println(sort(testData));
+    }
+
+    public static ArrayList<Integer> sort(ArrayList<Integer> dataList) {
+
+        boolean swap = false;
+        for(int i =0; i <= dataList.size() -1; i ++){
+
+            for(int j= 0; j< dataList.size() -1 -i; j++){
+                if(dataList.get(j) > dataList.get(j+1)){
+                    Collections.swap(dataList, j, j+1);
+                    swap = true;
+                }
+            }
+
+            if(swap == false)
+                break;
+        }
+
+        return dataList;
+    }
+}
