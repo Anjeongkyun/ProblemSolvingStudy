bbda29bcbfedd3111add8e8598f3fb7cf96effbe
dkswjdrbs123 <dkswjdrbs12@naver.com>
2022-07-11 12:31:58 +0900

    ACM-ICPC 2001 알고리즘 문제 풀이

--- Recursion/ACM_ICPC2001.java
+++ Recursion/ACM_ICPC2001.java
@@ -1,12 +1,19 @@
 package Recursion;
 
+import java.util.Scanner;
+
 public class ACM_ICPC2001 {
     //1, 2, 4, 7, 13, ...
     // f(n) = f(n-1) + f(n-2) + f(n-3) 패턴
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
-        System.out.println(recursion(4));
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(int i = 0; i< num; i ++){
+            int N = sc.nextInt();
+            System.out.println(recursion(N));
+        }
     }
 
     private static int recursion(int n){
@@ -16,6 +23,8 @@ public class ACM_ICPC2001 {
             return 2;
         }else if(n ==3) {
             return 4;
+        }else if (n <= 0 ){
+            return 0;
         }else{
             return recursion(n-3) + recursion(n-2) + recursion(n-1);
         }
