1a72f2e1df769e96a2ee27b6ce9fea89adbb0c15
dkswjdrbs123 <dkswjdrbs12@naver.com>
2022-07-12 09:51:13 +0900

    피보나치 동적 계획법(DP)을 이용한 풀이

---
+++ Recursion/Fibonacci_DP.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class Fibonacci_DP {
+    public static void main(String[] args) {
+
+        System.out.println(dp(9));
+    }
+
+    private static int dp(int n){
+        int[] cache = new int[n + 1];
+        cache[0] = 0;
+        cache[1] = 1;
+        for(int i = 2; i< n + 1; i++){
+            cache[i] = cache[i - 1] + cache[i - 2];
+        }
+        return cache[n];
+    }
+}
