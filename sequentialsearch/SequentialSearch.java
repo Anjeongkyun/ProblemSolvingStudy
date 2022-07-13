c1eb0124ecb01fe1296a75dcac0d034eb990a1a5
dkswjdrbs123 <dkswjdrbs12@naver.com>
2022-07-13 09:53:12 +0900

    순차 정렬 작성

---
+++ sequentialsearch/SequentialSearch.java
@@ -0,0 +1,28 @@
+package sequentialsearch;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SequentialSearch {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<20; i++)
+            list.add(i);
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        System.out.println(searchFunc(list, num));
+    }
+
+    private static int searchFunc(ArrayList<Integer> list, int searchItem){
+
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i) == searchItem)
+                return i;
+        }
+        return -1;
+    }
+}
