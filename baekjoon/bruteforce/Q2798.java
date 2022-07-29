d9391161935cc4c23e0edbd965cf77cb0da30d3d
dkswjdrbs123 <dkswjdrbs12@naver.com>
2022-07-29 11:10:02 +0900

    Q2798 블랙잭 로직 수정하여 실행 메모리 축소

--- baekjoon/bruteforce/Q2798.java
+++ baekjoon/bruteforce/Q2798.java
@@ -1,9 +1,11 @@
 package baekjoon.bruteforce;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Q2798 {
 
@@ -11,37 +13,48 @@ public class Q2798 {
     static int[] inputArr;
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         input();
         solve();
     }
 
-    static void input() {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine()," ");
+
+        N = Integer.parseInt(token.nextToken());
+        M = Integer.parseInt(token.nextToken());
         inputArr = new int[N];
 
+        token = new StringTokenizer(br.readLine()," ");
         for (int i = 0; i < N; i++) {
-            inputArr[i] = sc.nextInt();
+            inputArr[i] = Integer.parseInt(token.nextToken());
         }
+
+        br.close();
     }
 
     static void solve() {
         List<Integer> list = new ArrayList<Integer>();
+        int answer = -1;
 
         for (int i = 0; i < N - 2; i++) {
+            if(inputArr[i] >= M) continue;
             for(int j = i+ 1; j < N -1; j++){
+                if(inputArr[i] + inputArr[j] >= M) continue;
                 for(int k = j + 1; k < N; k++){
-                    if((inputArr[i] + inputArr[j] + inputArr[k]) <= M){
-                        list.add(inputArr[i] + inputArr[j] + inputArr[k]);
+                    int sum = (inputArr[i] + inputArr[j] + inputArr[k]);
+                    if(M == sum){
+                        answer = M;
+                    }
+
+                    if(answer < sum && sum < M){
+                        answer = sum;
                     }
                 }
             }
         }
-
-        int max = Collections.max(list);
-        System.out.println(max);
+        System.out.println(answer);
 
     }
 }
