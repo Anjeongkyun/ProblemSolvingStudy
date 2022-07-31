d975c70b4014d40b7606d85cd085ebe0e5574251
Anjeongkyun <dkswjdrbs12@naver.com>
2022-07-31 15:53:51 +0900

    [문자열] Q10809 알파벳찾기 풀이

---
+++ baekjoon/string/Q10809.java
@@ -0,0 +1,42 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q10809 {
+
+    static String word;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+    }
+
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        word = br.readLine();
+
+        arr = new int[26];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = -1;
+        }
+
+        br.close();
+    }
+
+    static void solve(){
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+
+            if(arr[ch - 'a'] == -1){
+                arr[ch - 'a'] = i;
+            }
+        }
+
+        for(int val : arr){
+            System.out.print(val + " ");
+        }
+    }
+}
