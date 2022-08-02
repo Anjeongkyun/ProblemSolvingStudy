8f85b97cb34b586941abcd5fbc59456fb979a16b
Anjeongkyun <dkswjdrbs12@naver.com>
2022-08-03 00:11:02 +0900

    [집합] Q10815 숫자 카드 풀이

--- baekjoon/map/Q10815.java
+++ baekjoon/map/Q10815.java
@@ -1,7 +1,5 @@
 package baekjoon.map;
 
-import com.sun.deploy.util.ArrayUtil;
-
 import java.io.*;
 import java.util.*;
 
@@ -9,41 +7,63 @@ import java.util.*;
 public class Q10815 {
     static BufferedReader br;
     static BufferedWriter bw;
-    static StringBuilder sb;
     static StringTokenizer st;
-    static int[] oneArr;
-    static int[] twoArr;
+    static List<Integer> oneArr;
     static int N, M;
 
 
     public static void main(String[] args) throws IOException {
         input();
-
+        solve();
+        close();
     }
     static void input() throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        sb = new StringBuilder();
-
         N = Integer.parseInt(br.readLine());
-        oneArr = new int[N];
-        st = new StringTokenizer(br.readLine()," ");
+        oneArr = new ArrayList<Integer>();
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            oneArr[i] = Integer.parseInt(st.nextToken());
+            oneArr.add(Integer.parseInt(st.nextToken()));
         }
+
+        Collections.sort(oneArr);
 
         M = Integer.parseInt(br.readLine());
-        twoArr = new int[M];
-        st = new StringTokenizer(br.readLine()," ");
-        for (int i = 0; i < M; i++) {
-            twoArr[i] = Integer.parseInt(st.nextToken());
-        }
+        st = new StringTokenizer(br.readLine());
 
-        br.close();
     }
-    static boolean searchFunc(int[] arr, int searchItem){
-        Arrays.sort(oneArr);
 
+    static void solve() throws IOException {
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if(searchFunc(num))
+                bw.write("1 ");
+            else
+                bw.write("0 ");
+        }
+    }
+
+    static boolean searchFunc(int searchItem){
+        int leftIdx = 0;
+        int rightIdx = N - 1;
+
+        while (leftIdx <= rightIdx) {
+            int mIdx = (leftIdx + rightIdx) / 2;
+            int mVal = oneArr.get(mIdx);
+
+            if(searchItem < mVal)
+                rightIdx = mIdx - 1;
+            else if(searchItem > mVal)
+                leftIdx = mIdx + 1;
+            else
+                return true;
+        }
         return false;
+    }
+
+    static void close() throws IOException {
+        br.close();
+        bw.close();
     }
 }
