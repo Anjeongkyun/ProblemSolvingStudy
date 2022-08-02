be35cb4dbecfa8a5b431a987426a1613af3d96c1
dkswjdrbs123 <dkswjdrbs12@naver.com>
2022-08-02 16:52:00 +0900

    [집합] Q10815 숫자 카드 -> 시간 초과

--- baekjoon/map/Q10815.java
+++ baekjoon/map/Q10815.java
@@ -1,46 +1,51 @@
 package baekjoon.map;
 
+import com.sun.deploy.util.ArrayUtil;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
+
 
 public class Q10815 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(br.readLine());
         int[] oneArr = new int[N];
 
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
         for (int i = 0; i < N; i++) {
             oneArr[i] = Integer.parseInt(st.nextToken());
         }
 
 
-        st = new StringTokenizer(br.readLine());
-        int M = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine()," ");
+        int M = Integer.parseInt(br.readLine());
         int[] twoArr = new int[M];
 
+        st = new StringTokenizer(br.readLine()," ");
         for (int i = 0; i < M; i++) {
             twoArr[i] = Integer.parseInt(st.nextToken());
         }
 
         for (int i = 0; i < M; i++) {
-            for(int j = 0; i< N; j++){
-                if(Arrays.asList(twoArr).contains(oneArr[j])){
-                    sb.append(1).append(' ');
-                }else{
-                    sb.append(0).append(' ');
+            boolean isCheck = false;
+            for (int j = 0; j < N; j++) {
+                if (twoArr[i] == oneArr[j]) {
+                    isCheck = true;
+                    break;
                 }
             }
+
+            if(isCheck){
+                sb.append(1).append(' ');
+            }else{
+                sb.append(0).append(' ');
+            }
         }
+        System.out.println(sb.toString());
 
     }
 }
