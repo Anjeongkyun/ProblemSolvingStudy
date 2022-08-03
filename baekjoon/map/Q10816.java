9288de505ff02bab9eeee4f4ba300aaf1d4ac9a9
Anjeongkyun <dkswjdrbs12@naver.com>
2022-08-03 23:17:05 +0900

    [집합] 백준 숫자카드2 풀이중

---
+++ baekjoon/map/Q10816.java
@@ -0,0 +1,76 @@
+package baekjoon.map;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+
+public class Q10816 {
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
+    static List<Integer> oneArr;
+    static int N, M;
+
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+        close();
+    }
+    static void input() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(br.readLine());
+        oneArr = new ArrayList<Integer>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            oneArr.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(oneArr);
+
+        M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+    }
+
+    static void solve() throws IOException {
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            searchFunc(num);
+        }
+    }
+
+    static int searchFunc(int searchItem){
+        int leftIdx = 0;
+        int rightIdx = N - 1;
+        int num = 0;
+
+        while (leftIdx <= rightIdx) {
+            int mIdx = (leftIdx + rightIdx) / 2;
+            int mVal = oneArr.get(mIdx);
+
+            if(mVal < oneArr.get(mIdx-1) && mVal > oneArr.get(mIdx-1)){
+                if(mVal < oneArr.get(mIdx-1)){
+                    rightIdx = mIdx - 1;
+                    num ++;
+                }else if (mVal > oneArr.get(mIdx-1)){
+                    leftIdx = mIdx + 1;
+                    num ++;
+                }
+            }else{
+
+            }
+        }
+
+        return num;
+    }
+
+    static void close() throws IOException {
+        br.close();
+        bw.close();
+    }
+}
