6d366733cfc36f45496afe1f653afa890c56d875
dkswjdrbs123 <dkswjdrbs12@naver.com>
2022-08-05 17:12:05 +0900

    Q7795 진행중2

--- baekjoon/binarySearch/Q7795먹을것인가먹힐것인가.java
+++ baekjoon/binarySearch/Q7795먹을것인가먹힐것인가.java
@@ -11,60 +11,57 @@ public class Q7795먹을것인가먹힐것인가 {
     static BufferedReader br;
     static StringTokenizer st;
 
-    static List<Integer> nmList;
-    static List<Integer> nList;
-    static List<Integer> mList;
+    static int[] nList;
+    static int[] mList;
 
     public static void main(String[] args) throws IOException {
-        input();
-        solve();
-        close();
-    }
 
-    static void input() throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
-        nmList = new ArrayList<>();
-        nList = new ArrayList<>();
-        mList = new ArrayList<>();
-
         T = Integer.parseInt(br.readLine());
-    }
 
-    static void solve() throws IOException {
-        for (int i = 0; i < T; i++) {
-            int cnt = 0;
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < 2; j++) {
-                nmList.add(Integer.parseInt(st.nextToken()));
-            }
-
-            N = nmList.get(0);
-            M = nmList.get(1);
-
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int k = 0; k < N; k++) {
-                nList.add(Integer.parseInt(st.nextToken()));
-            }
-
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int k = 0; k < M; k++) {
-                mList.add(Integer.parseInt(st.nextToken()));
-            }
-
-            Collections.sort(nList);
-            int midIdx = mList.size() / 2;
-            for (int l = 0; l < mList.size(); l++) {
-                if(nList.get(l) <= mList.get(midIdx))
-                    continue;
-                else if (nList.get(l) > mList.get(midIdx))
-                    cnt ++;
-            }
-
-            System.out.println(cnt);
+        for (int i = 1; i <= T; i++) {
+            input();
+            solve();
         }
     }
 
-    static void close() throws IOException {
-        br.close();
+    static void input() throws IOException {
+        st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nList = new int[N+1];
+        mList = new int[M+1];
+
+        for (int i = 1; i <= N; i++) {
+            nList[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= M; i++) {
+            mList[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    static void solve(){
+        Arrays.sort(mList, 1, M+1);
+
+        int ans = 0;
+        for (int i = 0; i <= N; i++) {
+            ans += lowerBound(nList, 1, M, nList[i]);
+        }
+        System.out.println(ans);
+    }
+
+    static int lowerBound(int[] nList, int L, int R, int X){
+        int res = L - 1;
+        while(L <= R){
+            int mid = (L + R) / 2;
+            if(nList[mid] < X){
+                res = mid;
+                L = mid + 1;
+            }else {
+                R = mid - 1;
+            }
+        }
+        return res;
     }
 }
