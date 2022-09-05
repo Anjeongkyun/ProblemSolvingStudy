47ddc1ec53abff1563519f0f99e58c7aa24bf05b
Anjeongkyun <dkswjdrbs12@naver.com>
2022-09-05 22:19:50 +0900

    feat: 코딜리티 TapeEquilibrium 문제풀이 (100%)

--- codility/L3_TapeEquilibrium.java
+++ codility/L3_TapeEquilibrium.java
@@ -1,29 +1,23 @@
 package codility;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class L3_TapeEquilibrium {
     public static void main(String[] args) {
         int[] candArr = {3, 1, 2, 4, 3};
-//        Math.abs(-1);
 
-        List<Integer> list = new ArrayList<>();
-        int calculVal = -1;
-        boolean isFirst = true;
+        int sumAllVal = 0;
+        for(int cand : candArr)
+            sumAllVal += cand;
+
+        int minDiff = Integer.MAX_VALUE;
+        int currentDiff = Integer.MAX_VALUE;
+        int sumLeftVal = 0;
+        int sumRightVal = 0;
         for (int i = 0; i < candArr.length - 1; i++) {
-            int[] oneArr = Arrays.copyOfRange(candArr, 0, i + 1);
-            int oneSum = Arrays.stream(oneArr).sum();
-
-            int[] twoArr = Arrays.copyOfRange(candArr, i + 1, candArr.length);
-            int twoSum = Arrays.stream(twoArr).sum();
-
-            list.add(Math.abs(oneSum - twoSum));
+            sumLeftVal += candArr[i];
+            sumRightVal = sumAllVal - sumLeftVal;
+            currentDiff = Math.abs(sumLeftVal - sumRightVal);
+            minDiff = Math.min(currentDiff, minDiff);
         }
-
-        System.out.println(Arrays.asList(list));
-        System.out.println(Collections.min(list));
+        System.out.println(minDiff);
     }
 }
